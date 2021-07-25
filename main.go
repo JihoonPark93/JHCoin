07e205f86bb43ffa4372e9b2e67eef371042a213
Your Name <you@your-domain.com>
2021-07-25 14:00:34 +0900

    golang 인강 7.3 완료

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/JihoonPark93/JHCoin/explorer"
 	"github.com/JihoonPark93/JHCoin/rest"
 )
 
+func usage() {
+	fmt.Printf("Welcome to JH conin\n\n")
+	fmt.Printf("Please use the following command :\n\n")
+	fmt.Printf("-port:	Set the PORT of the server\n")
+	fmt.Printf("-mode:	Choose between 'html' and 'rest'\n")
+	os.Exit(0)
+}
+
 func main() {
-	go explorer.Start(3000)
-	rest.Start(4000)
+	if len(os.Args) == 1 {
+		usage()
+	}
+
+	port := flag.Int("port", 4000, "Set port of the server")
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
+
+	flag.Parse()
+
+	fmt.Println(*port, *mode)
+	switch *mode {
+	case "html":
+		rest.Start(*port)
+	case "rest":
+		explorer.Start(*port)
+	default:
+		usage()
+	}
 }
--- rest/rest.go
+++ rest/rest.go
@@ -63,13 +63,11 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			Description: "See A block",
 		},
 	}
-	rw.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(rw).Encode(data)
 }
 func blocks(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		rw.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
 	case "POST":
 		// {"data": "my block data"}
@@ -94,9 +92,17 @@ func block(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func jsonContentTypepMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(rw, r)
+	})
+}
+
 func Start(aPort int) {
-	router := mux.NewRouter()
 	port = fmt.Sprintf(":%d", aPort)
+	router := mux.NewRouter()
+	router.Use(jsonContentTypepMiddleware)
 	router.HandleFunc("/", documentation).Methods("GET")
 	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
 	router.HandleFunc("/blocks/{height:[0-9]+}", block).Methods("GET")
